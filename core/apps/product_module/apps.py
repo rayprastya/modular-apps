0d30e572280cd671cf4a4511b335b2c3bc90d5bd
rayprastya <rayhan.prastya@gmail.com>
2025-05-19 21:18:53 +0700

    change modular to product module

--- core/apps/product_module/apps.py
+++ core/apps/product_module/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ModularConfig(AppConfig):
+class ProductModuleConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'modular'
+    name = 'product_module'
