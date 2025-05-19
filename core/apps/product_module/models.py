aeba66c2a9204f6e5963b7058e163f344ddf0de1
rayprastya <rayhan.prastya@gmail.com>
2025-05-19 22:16:29 +0700

    feat: models for modular engine and product

--- core/apps/product_module/models.py
+++ core/apps/product_module/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    barcode = models.CharField(max_length=25)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
