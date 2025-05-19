24eb49fec70ee3e22ea9bb51d819847b59d0e269
rayprastya <rayhan.prastya@gmail.com>
2025-05-20 01:14:33 +0700

    feat: add slugs for module unique name

--- core/apps/modular_engine/models.py
+++ core/apps/modular_engine/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.utils.text import slugify
 from core.utils.models import BaseModel
+
 class Module(BaseModel):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
     description = models.TextField()
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
-
-
-
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
