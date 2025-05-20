fab15f0ad46a1937e9529f9220d52caa05359c8b
rayprastya <rayhan.prastya@gmail.com>
2025-05-20 12:18:21 +0700

    fix url path

--- core/apps/modular_engine/urls.py
+++ core/apps/modular_engine/urls.py
@@ -3,6 +3,6 @@ from .views import ModularView, ModuleActionView
 
 urlpatterns = [
     path('modular-tools', ModularView, name='modular-tools'),
-    path('install/<str:slug/', ModuleActionView, name='module-action'),
+    path('install/<str:slug>/', ModuleActionView, name='module-action'),
 ]
 
