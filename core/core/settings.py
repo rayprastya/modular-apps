f4f16782bcace889fffec6dad5da9f99cdb12c97
rayprastya <rayhan.prastya@gmail.com>
2025-05-20 12:09:43 +0700

    feat: constant to gather value from env, change db from sqlite to use pgsql

--- core/core/settings.py
+++ core/core/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import core.utils.constant as constant
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': constant.NAME,
+        'USER': constant.USER,
+        'PASSWORD': constant.PASSWORD,
+        'HOST': constant.HOST,
+        'PORT': constant.PORT,
     }
 }
 
---
+++ core/core/utils/constant.py
@@ -0,0 +1,10 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+NAME=os.getenv('NAME')
+USER=os.getenv('USER')
+PASSWORD=os.getenv('PASSWORD')
+HOST=os.getenv('HOST')
+PORT=os.getenv('PORT')
