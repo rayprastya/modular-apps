99e984113c36c26289096d2a2bd5855779ceab2c
rayprastya <rayhan.prastya@gmail.com>
2025-05-20 15:01:32 +0700

    fix: url cbv to add as_view

--- core/apps/modular_engine/urls.py
+++ core/apps/modular_engine/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import ModularView, ModuleActionView
 
 urlpatterns = [
-    path('modular-tools', ModularView, name='modular-tools'),
-    path('install/<str:slug>/', ModuleActionView, name='module-action'),
+    path('module/', ModularView.as_view(), name='modular-tools'),
+    path('module/install/<str:slug>/', ModuleActionView.as_view(), name='module-action'),
 ]
 
--- core/apps/product_module/urls.py
+++ core/apps/product_module/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('product-module', views.ProductListView.as_view(), name='product-module'),
+    path('product-module/', views.ProductListView.as_view(), name='product-module'),
     path('product-module/create/', views.ProductCreateView.as_view(), name='product-create'),
     path('product-module/edit/<int:pk>/', views.ProductUpdateView.as_view(), name='product-edit'),
     path('product-module/delete/<int:pk>/', views.ProductDeleteView.as_view(), name='product-delete'),
