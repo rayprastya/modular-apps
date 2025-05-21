b52f6c57222317338d8c94dfd729ec66f0747a06
rayprastya <rayhan.prastya@gmail.com>
2025-05-21 21:59:31 +0700

    disable debug for production

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7p9p5l-og#$8p-s)ks&ti!3@mgk&+*h%c@0ymbp=b^73)si$@#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['145.79.12.81', 'localhost', '127.0.0.1']
 
